9fd01ba084628633ed6f6b0c0c7dcc4dc21e06c5
ImRaabe <raabees@mail.uc.edu>
2024-02-11 13:01:32 -0500

    Project init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner in = new Scanner(System.in);
+    System.out.println("Please enter the cost of your item");
+    float itemCost = in.nextFloat();
+    float shippingCost;
+
+    if(itemCost >= 100)
+    {
+        shippingCost = 0;
+    }
+    else
+    {
+        shippingCost = (float) (0.02 * itemCost);
+    }
+    float totalCost = itemCost + shippingCost;
+    System.out.print("The calculated shipping cost for your item is: $" + shippingCost + " and the total cost is: $" + totalCost);
     }
 }
